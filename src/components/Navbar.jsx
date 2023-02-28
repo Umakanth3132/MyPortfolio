0778b4aeb0db9983d9fb3132b69383caae4c87ed
Umakanth <umakanthreddy.m@gmail.com>
2023-02-28 15:49:06 -0500

    added icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import {FaBars, FaTimes,FaGithub,FaLinkedin, FaFacebook} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import UMAKANTH from '../assests/UMAKANTH.png'
-import LOGO from '../assests/1.png'
+import LOGO from '../assests/uk.png'
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
 
@@ -16,11 +17,23 @@ const Navbar = () => {
         </div>
        
        <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li>
+            <Link  to="home"  smooth={true}  duration={500} >
+          Home
+        </Link>
+            </li>
+            <li><Link  to="about"  smooth={true}  duration={500} >
+          About
+        </Link></li>
+            <li><Link  to="skills"  smooth={true}  duration={500} >
+          Skills
+        </Link></li>
+            <li><Link  to="work"  smooth={true}  duration={500} >
+          Work
+        </Link></li>
+            <li><Link  to="contact"  smooth={true}  duration={500} >
+          Contact
+        </Link></li>
         </ul>
       
 
@@ -29,19 +42,30 @@ const Navbar = () => {
         {!nav ? <FaBars></FaBars>:<FaTimes></FaTimes>}
        </div>
 
-       <ul className={!nav ? 'hidden':'absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center text-black'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Work</li>
-            <li className='py-6 text-4xl'>Contact</li>
+       <ul className={!nav ? 'hidden':'absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center bg-[#0a192f]'}>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="home"  smooth={true}  duration={500} >
+          Home
+        </Link></li>
+            <li  className='py-6 text-4xl'><Link onClick={handleClick} to="about"  smooth={true}  duration={500} >
+          About
+        </Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="skills"  smooth={true}  duration={500} >
+          Skills
+        </Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="work"  smooth={true}  duration={500} >
+          Work
+        </Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="contact"  smooth={true}  duration={500} >
+          Skills
+        </Link></li>
         </ul>
 
        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                 <a  className='flex justify-between items-center w-full text-black'
-                href="/">
+                href="https://www.linkedin.com/in/linkedin.com/in/umakanth3132-job-profile/" target="_blank">
                 Linkedin<FaLinkedin size={30}/>
 
                 </a>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,44 +1,103 @@
 import React from 'react'
+import java from '../assests/java.png'
+import hiberbate from '../assests/hibernate.png'
+import spring from '../assests/spring.png'
+import microservice from '../assests/Microservice.png'
+import mongo from '../assests/mongo.png'
+import github from '../assests/github.png'
+import html from '../assests/html.png'
+import css from '../assests/css.png'
+import react from '../assests/react.png'
+import node from '../assests/node.png'
+import js from '../assests/javascript.png'
+import bootstrap from '../assests/Bootstrap.png'
+import tailwind from '../assests/tailwind.png'
+import aws from '../assests/aws.png'
 
 const Skills = () => {
   return (
     <div name='skills' className='bg-[#0a192f]  text-gray-300 '>
-        <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center w-full h-full gap-8'>
+        <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center w-full h-full '>
             <div >
                 <p className='  text-4xl font-bold inline border-b-4 border-pink-600 '>Skill Set</p>
-                <p className='py-4'> Back-end Technology</p>
+                <p className='py-4'>Back-end Technology</p>
             </div>
-        </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto'></img>
+                <img className='w-20 mx-auto' src={java} alt="JAVA icon"></img>
+                <p className='my-4'>JAVA</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={hiberbate} alt="HIBERNATE icon"></img>
+                <p className='my-4'>HIBERNATE</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={spring} alt="spring boot icon"></img>
+                <p className='my-4'>SPRING BOOT</p>
+            </div>
+        
+       
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={microservice} alt="MICROSERVICE icon"></img>
+                <p className='my-4'>MICROSERVICE</p>
+            </div>
+       
+        
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={mongo} alt="MICROSERVICE icon"></img>
+                <p className='my-4'>MONGO DB</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={github} alt="GITHUB icon"></img>
+                <p className='my-4'>GITHUB</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={aws} alt="AWS icon"></img>
+                <p className='my-4'>AWS</p>
+            </div>
+            </div>
+            <div >
+                
+                <p className='py-4'>Front-end Technology</p>
+            </div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+            
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={html} alt="HTML icon"></img>
                 <p className='my-4'>HTML</p>
             </div>
-        </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto'></img>
-                <p className='my-4'>HTML</p>
+                <img className='w-20 mx-auto' src={css} alt="CSS icon"></img>
+                <p className='my-4'>CSS</p>
             </div>
-        </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto'></img>
-                <p className='my-4'>HTML</p>
+                <img className='w-20 mx-auto' src={js} alt="js icon"></img>
+                <p className='my-4'>JAVASCRIPT</p>
             </div>
-        </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto'></img>
-                <p className='my-4'>HTML</p>
+                <img className='w-20 mx-auto' src={react} alt="REACT icon"></img>
+                <p className='my-4'>REACT JS</p>
             </div>
-        </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto'></img>
-                <p className='my-4'>HTML</p>
+                <img className='w-20 mx-auto' src={node} alt="NODE icon"></img>
+                <p className='my-4'>NODE JS</p>
             </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={bootstrap} alt="BOOTSTRAP icon"></img>
+                <p className='my-4'>BOOTSTRAP</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={tailwind} alt="TAILWIND icon"></img>
+                <p className='my-4'>TAILWIND</p>
+            </div>
+
+            </div>
+        
+        
+            
+       
         </div>
+        
 
     </div>
   )
